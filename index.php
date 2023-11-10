fa5edffc649eb080364ecca9680632c7471bc2d1
sawaiii <vacheslav20013@yandex.ru>
2023-11-10 06:59:45 +0300

    туц туц туц

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Здарова кабан</title>
+</head>
+<body>
 <?php
-    $a = 341; // Подставить первоначальное значение
-    
-    $a1 = $a/2;
-    $percent = 65;
-    $num_percent = $a /100 * $percent;
-    $b = $a + $num_percent;
-    $b1 = $b /2;
-    $percent1 = 220;
-    $num_percent1 = $a / 100* $percent1;
-    $c = $a + $num_percent1;
-    $c1 = $c /2;
-    echo "Комплект №1 - Цена первый день: " . round($a,-1) . "\n";
-    echo "Комплект №1 - Цена второй день: " . round($a1,-1) . "\n";
-    echo "Комплект №2 - Цена первый день: " . round($b,-1) . "\n";
-    echo "Комплект №2 - Цена второй день: " . round($b1,-1) . "\n";
-    echo "Комплект №3 - Цена первый день: " . round($c,-1) . "\n";
-    echo "Комплект №3 - Цена второй день: " . round($c1,-1) . "\n";
-?>
+
+if( $_GET['price']) {
+    require('main.php');
+    exit();
+ };
+ ?>
+ 
+    <form action = "php $_PHP_SELF " method = "GET">
+         Цена: <input type = "text" name = "price" >
+        <input type = "submit" >
+   </form>
+
+</body>
+</html>
